a8668f463b9f7b5097bbdd8fa0850c42c70c0fa2
Yusuf347 <yusuf.cheik.mostafa@gymhum.de>
2023-10-06 08:54:45 +0200

    zsgdfi
    hzdfeuwf

--- JavaSpringVorlage-main/src/main/java/gymhum/de/RsaVerfahrenController.java
+++ JavaSpringVorlage-main/src/main/java/gymhum/de/RsaVerfahrenController.java
@@ -1,22 +1,6 @@
 package gymhum.de;
 
-import java.math.BigInteger;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-
 public class RsaVerfahrenController {
     
-    BigInteger p;
-    BigInteger q;
-    BigInteger n;
-    BigInteger m;
-    BigInteger d;
-    BigInteger e;
-    boolean keysequal;
-
+    
 }
