3b1cd5491093a03207e7dc1fd1113f4ff3102abf
TimothyWilckens <timothy.wilckens@gymhum.de>
2023-10-11 11:18:54 +0200

    smallChangeController

--- JavaSpringVorlage-main/src/main/java/gymhum/de/RsaVerfahrenController.java
+++ JavaSpringVorlage-main/src/main/java/gymhum/de/RsaVerfahrenController.java
@@ -1,14 +1,13 @@
 package gymhum.de;
 
 import java.math.BigInteger;
-//test
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+@Controller
 public class RsaVerfahrenController {
     BigInteger p;
     BigInteger q;
