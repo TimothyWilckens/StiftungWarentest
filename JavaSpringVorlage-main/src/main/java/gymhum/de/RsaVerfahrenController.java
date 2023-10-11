4459ce080f46443f10819b5d19be923cdc200dc0
Yusuf347 <yusuf.cheik.mostafa@gymhum.de>
2023-10-11 11:05:52 +0200

    test

--- JavaSpringVorlage-main/src/main/java/gymhum/de/RsaVerfahrenController.java
+++ JavaSpringVorlage-main/src/main/java/gymhum/de/RsaVerfahrenController.java
@@ -10,13 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 public class RsaVerfahrenController {
-        BigInteger p;
+    BigInteger p;
     BigInteger q;
     BigInteger n;
     BigInteger m;
     BigInteger d;
     BigInteger e;
     boolean keysequal;
+
+    
 
 public RsaVerfahrenController(){
         setP(BigInteger.valueOf(13));
@@ -25,6 +27,26 @@ public RsaVerfahrenController(){
            
     }
 
+    @GetMapping("/rsaverahren")
+    public String rsaverfahren(@RequestParam(name="activePage", required = false, defaultValue = "rsaverfahren") String activePage, Model model){
+        model.addAttribute("activePage", "rsaverfahren");
+        return "index.html";
+    }
+    @GetMapping("/rsaverfahren_choose_p_and_q")
+    public String diffiehellman_choose_p_and_q(@RequestParam(name="activePage", required = false, defaultValue = "rsavefahren") String activePage, Model model){
+        model.addAttribute("activePage", "rsaverfahren_choose_p_and_q");
+        return "index.html";
+    }
+    @GetMapping("/rsavefahren_choose_p_and_q_error")
+    public String rsaverfahren_choose_p_and_q_error(@RequestParam(name="activePage", required = false, defaultValue = "rsaverfahren") String activePage, Model model){
+        model.addAttribute("activePage", "rsaverfahren_p_and_q_error");
+        return "index.html";
+    }
+    
+
+    
+
+          
     public void setP(BigInteger p) {
         this.p = p;
     }
@@ -65,16 +87,6 @@ public RsaVerfahrenController(){
     public BigInteger getE() {
         return e;
     }
-
-
-
-
-
-
-
-    
-
-
     
 }
 
