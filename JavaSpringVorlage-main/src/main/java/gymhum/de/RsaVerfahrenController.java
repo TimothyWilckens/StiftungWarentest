af551b9ae6c0c7e4360ea3f2e515e923cfda2bd6
Yusuf347 <yusuf.cheik.mostafa@gymhum.de>
2023-10-11 10:02:59 +0200

    test

--- JavaSpringVorlage-main/src/main/java/gymhum/de/RsaVerfahrenController.java
+++ JavaSpringVorlage-main/src/main/java/gymhum/de/RsaVerfahrenController.java
@@ -1,15 +1,87 @@
  package gymhum.de;
 
 import java.math.BigInteger;
+//test
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 public class RsaVerfahrenController {
-     BigInteger p;
+    BigInteger p;
     BigInteger q;
     BigInteger n;
     BigInteger m;
     BigInteger d;
     BigInteger e;
     boolean keysequal;
+
+    public RsaVerfahrenController(){
+        setP(BigInteger.valueOf(13));
+        setQ(BigInteger.valueOf(7));
+        setE(BigInteger.valueOf(11));
+           
+    }
+
+    public void setP(BigInteger p) {
+        this.p = p;
+    }
+    public BigInteger getP() {
+        return p;
+    }
+
+    public void setQ(BigInteger q) {
+        this.q = q;
+    }
+    public BigInteger getQ() {
+        return q;
+    }
+
+    public void setD(BigInteger d) {
+        this.d = d;
+    }
+    public BigInteger getD() {
+        return d;
+    }
+    public void setN(BigInteger n) {
+        this.n = n;
+    }
+    public BigInteger getN() {
+        return n;
+    }
+
+    public void setM(BigInteger m) {
+        this.m = m;
+    }
+    public BigInteger getM() {
+        return m;
+    }
+
+    public void setE(BigInteger e) {
+        this.e = e;
+    }
+    public BigInteger getE() {
+        return e;
+    }
+
+
+
+
+
+
+
     
+
+
     
 }
+
+
+
+
+
+
+
+
