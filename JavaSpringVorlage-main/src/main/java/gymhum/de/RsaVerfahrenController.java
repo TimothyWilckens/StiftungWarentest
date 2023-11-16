85e7b5bf4109058de545ccb48826aba0822fe98a
FinnPogO <111505081+FinnPogO@users.noreply.github.com>
2023-11-16 17:25:19 +0100

    rsa verfahren klartext

--- JavaSpringVorlage-main/src/main/java/gymhum/de/RsaVerfahrenController.java
+++ JavaSpringVorlage-main/src/main/java/gymhum/de/RsaVerfahrenController.java
@@ -1,6 +1,7 @@
 package gymhum.de;
 
 import java.math.BigInteger;
+import java.util.Scanner;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,57 +18,58 @@ public class RsaVerfahrenController {
     BigInteger e;
     boolean keysequal;
 
-    
-
-public RsaVerfahrenController(){
+    public RsaVerfahrenController() {
         setP(BigInteger.valueOf(13));
         setQ(BigInteger.valueOf(7));
         setE(BigInteger.valueOf(11));
-           
     }
 
-    @GetMapping("/rsaverahren")
-    public String rsaverfahren(@RequestParam(name="activePage", required = false, defaultValue = "rsaverfahren") String activePage, Model model){
+    @GetMapping("/rsaverfahren")
+    public String rsaverfahren(@RequestParam(name = "activePage", required = false, defaultValue = "rsaverfahren") String activePage, Model model) {
         model.addAttribute("activePage", "rsaverfahren");
         return "index.html";
     }
+
     @GetMapping("/rsaverfahren_choose_p_and_q")
-    public String diffiehellman_choose_p_and_q(@RequestParam(name="activePage", required = false, defaultValue = "rsavefahren") String activePage, Model model){
+    public String diffiehellman_choose_p_and_q(
+            @RequestParam(name = "activePage", required = false, defaultValue = "rsavefahren") String activePage, Model model) {
         model.addAttribute("activePage", "rsaverfahren_choose_p_and_q");
         return "index.html";
     }
+
     @GetMapping("/rsavefahren_choose_p_and_q_error")
-    public String rsaverfahren_choose_p_and_q_error(@RequestParam(name="activePage", required = false, defaultValue = "rsaverfahren") String activePage, Model model){
+    public String rsaverfahren_choose_p_and_q_error(
+            @RequestParam(name = "activePage", required = false, defaultValue = "rsaverfahren") String activePage,
+            Model model) {
         model.addAttribute("activePage", "rsaverfahren_p_and_q_error");
         return "index.html";
     }
-    
+
     public void generateRSAKeys() {
         // Berechne n
         n = p.multiply(q);
-    
+
         // Berechne φ(n) (totient-Funktion)
         BigInteger phi = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
-    
+
         // Wähle d so, dass d*e ≡ 1 (mod φ(n))
         d = e.modInverse(phi);
     }
-    
+
     public BigInteger encrypt(BigInteger plaintext) {
         // Verschlüsselung: c = plaintext^e % n
         return plaintext.modPow(e, n);
     }
-    
+
     public BigInteger decrypt(BigInteger ciphertext) {
         // Entschlüsselung: plaintext = ciphertext^d % n
         return ciphertext.modPow(d, n);
     }
-    
- 
-          
+
     public void setP(BigInteger p) {
         this.p = p;
     }
+
     public BigInteger getP() {
         return p;
     }
@@ -75,6 +77,7 @@ public RsaVerfahrenController(){
     public void setQ(BigInteger q) {
         this.q = q;
     }
+
     public BigInteger getQ() {
         return q;
     }
@@ -82,12 +85,15 @@ public RsaVerfahrenController(){
     public void setD(BigInteger d) {
         this.d = d;
     }
+
     public BigInteger getD() {
         return d;
     }
+
     public void setN(BigInteger n) {
         this.n = n;
     }
+
     public BigInteger getN() {
         return n;
     }
@@ -95,6 +101,7 @@ public RsaVerfahrenController(){
     public void setM(BigInteger m) {
         this.m = m;
     }
+
     public BigInteger getM() {
         return m;
     }
@@ -102,17 +109,38 @@ public RsaVerfahrenController(){
     public void setE(BigInteger e) {
         this.e = e;
     }
+
     public BigInteger getE() {
         return e;
     }
-    
-    
+
+    public static void main(String[] args) {
+        RsaVerfahrenController rsaController = new RsaVerfahrenController();
+        rsaController.generateRSAKeys(); // Stelle sicher, dass die Schlüssel generiert wurden
+
+        // Benutzereingabe für den Text
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Gib den zu verschlüsselnden Text ein: ");
+        String plaintext = scanner.nextLine();
+
+        // Text verschlüsseln
+        BigInteger encryptedText = rsaController.encryptText(plaintext);
+
+        // Ausgabe des verschlüsselten Textes
+        System.out.println("Verschlüsselter Text: " + encryptedText);
+
+        // Schließe den Scanner
+        scanner.close();
+    }
+
+    // Methode, um einen Text in eine BigInteger umzuwandeln
+    public static BigInteger convertTextToBigInteger(String text) {
+        byte[] bytes = text.getBytes();
+        return new BigInteger(bytes);
+    }
+
+    // Methode, um Text mit dem öffentlichen Schlüssel zu verschlüsseln
+    public BigInteger encryptText(String text) {
+        return encrypt(convertTextToBigInteger(text));
+    }
 }
-
-
-
-
-
-
-
-
